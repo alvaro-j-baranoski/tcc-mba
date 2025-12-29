1e8f2196dea03d9a3c550df295d9287bea48d84e
Alvaro <alvarojosebaranoski01@gmail.com>
2025-12-29 16:55:57 -0300

    fix: Update client address

--- PGRFacil.UI/src/services/client.ts
+++ PGRFacil.UI/src/services/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://localhost:54085',
+  baseURL: 'https://localhost:51957',
   timeout: 1000,
   headers: {
     "Accept": 'text/plain',
