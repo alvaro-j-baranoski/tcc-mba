489fbe928205460d28b81bbfb322ead25c8e002b
Alvaro <alvarojosebaranoski01@gmail.com>
2026-01-04 12:37:18 -0300

    fix: Formato da URL

--- PGRFacil.UI/src/services/client.ts
+++ PGRFacil.UI/src/services/client.ts
@@ -4,7 +4,7 @@ import type { InternalAxiosRequestConfig } from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const client = axios.create({
-  baseURL: API_URL,
+  baseURL: `https://${API_URL}`,
   timeout: 1000,
   headers: {
     Accept: "text/plain",
