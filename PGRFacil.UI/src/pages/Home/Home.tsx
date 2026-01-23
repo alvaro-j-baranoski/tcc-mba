b35a2379cb59bc2da3c92e14cb74640e2e4dddb6
Alvaro <alvarojosebaranoski01@gmail.com>
2026-01-22 22:08:19 -0300

    feat: Adiciona spinner a matriz de risco

--- PGRFacil.UI/src/pages/Home/Home.tsx
+++ PGRFacil.UI/src/pages/Home/Home.tsx
@@ -4,10 +4,10 @@ import { RelatoriosService } from "@/services/RelatoriosService";
 import { RiskMatrix } from "@/components/RiskMatrix";
 import { QueryKeys } from "@/lib/utils";
 import ProgramasTable from "./components/ProgramasTable";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function Home() {
-
-  const { data: matrizDeRiscoData } = useQuery({
+  const { data: matrizDeRiscoData, isPending } = useQuery({
     queryKey: [QueryKeys.GetMatrizDeRisco],
     queryFn: RelatoriosService.getMatrizDeRisco,
     refetchOnWindowFocus: false,
@@ -17,7 +17,11 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 font-sans text-gray-900">
       <AppHeader />
-      {matrizDeRiscoData && <RiskMatrix data={matrizDeRiscoData.data} />}
+      {!isPending ? (
+        matrizDeRiscoData && <RiskMatrix data={matrizDeRiscoData.data} />
+      ) : (
+        <Spinner className="mx-auto my-10 size-8" />
+      )}
       <ProgramasTable />
     </div>
   );
