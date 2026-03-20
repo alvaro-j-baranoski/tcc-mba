fc00ba1e2d72ba9b165cd57c04ab3ec95fad060b
Alvaro <alvarojosebaranoski01@gmail.com>
2026-03-20 01:39:00 -0300

    feat: Adiciona fields no RiscoDto

--- PGRFacilAPI/PGRFacilAPI.Application/Risco/RiscoDto.cs
+++ PGRFacilAPI/PGRFacilAPI.Application/Risco/RiscoDto.cs
@@ -4,32 +4,43 @@ using PGRFacilAPI.Domain.Models;
 
 namespace PGRFacilAPI.Application.Risco
 {
-    public record RiscoDto(Guid Id, string Local, string Atividades, IEnumerable<string> Perigos, IEnumerable<string> Danos, AgentesDeRisco Agentes,
-        string TipoDeAvaliacao, uint Severidade, uint Probabilidade, uint Significancia, NivelSignificancia NivelSignificancia, PlanoDeAcaoDto? PlanoDeAcao = null)
+    public record RiscoDto
     {
+        public required Guid Id { get; init; }
+        public required string Local { get; init; }
+        public required string Atividades { get; init; }
+        public required IEnumerable<string> Perigos { get; init; }
+        public required IEnumerable<string> Danos { get; init; }
+        public required AgentesDeRisco Agentes { get; init; }
+        public required string TipoDeAvaliacao { get; init; }
+        public required uint Severidade { get; init; }
+        public required uint Probabilidade { get; init; }
+        public required uint Significancia { get; init; }
+        public required NivelSignificancia NivelSignificancia { get; init; }
+        public required PlanoDeAcaoDto? PlanoDeAcao { get; init; } = null;
+
         /// <summary>
         /// Maps a <see cref="RiscoEntity"/> to create a <see cref="RiscoDto"/>.
         /// </summary>
         public static RiscoDto From(RiscoEntity entity)
         {
-            PlanoDeAcaoDto? planoDeAcao = null;
-            if (entity.PlanoDeAcao is not null)
-            {
-                planoDeAcao = PlanoDeAcaoDto.From(entity.PlanoDeAcao);
-            }
+            PlanoDeAcaoDto? planoDeAcao = entity.PlanoDeAcao is null ? null : PlanoDeAcaoDto.From(entity.PlanoDeAcao);
 
-            return new RiscoDto(entity.Id,
-                entity.Local,
-                entity.Atividades,
-                entity.Perigos.Select(p => p.Descricao),
-                entity.Danos.Select(d => d.Descricao),
-                entity.Agentes,
-                entity.TipoDeAvaliacao,
-                entity.Severidade,
-                entity.Probabilidade,
-                entity.Significancia,
-                entity.NivelSignificancia,
-                planoDeAcao);
+            return new RiscoDto
+            {
+                Id = entity.Id,
+                Local = entity.Local,
+                Atividades = entity.Atividades,
+                Perigos = entity.Perigos.Select(p => p.Descricao),
+                Danos = entity.Danos.Select(d => d.Descricao),
+                Agentes = entity.Agentes,
+                TipoDeAvaliacao = entity.TipoDeAvaliacao,
+                Severidade = entity.Severidade,
+                Probabilidade = entity.Probabilidade,
+                Significancia = entity.Significancia,
+                NivelSignificancia = entity.NivelSignificancia,
+                PlanoDeAcao = planoDeAcao,
+            };
         }
     }
 }
