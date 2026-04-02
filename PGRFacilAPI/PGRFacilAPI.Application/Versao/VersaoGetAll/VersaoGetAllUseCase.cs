ffc1ef25e5a38da16241fc61e279d828dca1393b
Alvaro <alvarojosebaranoski01@gmail.com>
2026-04-02 01:36:12 -0300

    feat: Returns versions ordered

--- PGRFacilAPI/PGRFacilAPI.Application/Versao/VersaoGetAll/VersaoGetAllUseCase.cs
+++ PGRFacilAPI/PGRFacilAPI.Application/Versao/VersaoGetAll/VersaoGetAllUseCase.cs
@@ -11,14 +11,26 @@ namespace PGRFacilAPI.Application.Versao.VersaoGetAll
             await gheRepository.GetById(input.GheId);
 
             IEnumerable<VersaoEntity> versoes = await versaoRepository.GetAllByGheId(input.GheId);
-            List<VersaoDto> result = [];
-
-            foreach (VersaoEntity versao in versoes)
-            {
-                result.Add(new VersaoDto(versao.Id, versao.GheId, versao.Versao, versao.DataCriacao, versao.Observacoes));
-            }
+            List<VersaoDto> result = versoes
+                .OrderBy(v => v.Versao, new VersaoComparer())
+                .Select(v => new VersaoDto(v.Id, v.GheId, v.Versao, v.DataCriacao, v.Observacoes))
+                .ToList();
 
             return new VersaoGetAllOutputDto(result);
+        }
+
+        private sealed class VersaoComparer : IComparer<string>
+        {
+            public int Compare(string? x, string? y)
+            {
+                var partsX = (x ?? "0.0").Split('.');
+                var partsY = (y ?? "0.0").Split('.');
+
+                int majorCompare = int.Parse(partsX[0]).CompareTo(int.Parse(partsY[0]));
+                if (majorCompare != 0) return majorCompare;
+
+                return int.Parse(partsX[1]).CompareTo(int.Parse(partsY[1]));
+            }
         }
     }
 }
