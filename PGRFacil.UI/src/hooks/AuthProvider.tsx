71ecfa182188146ee532c649b763936b4d4a44b1
Alvaro <alvarojosebaranoski01@gmail.com>
2026-04-02 14:52:25 -0300

    feat: Refactor AuthProvider to improve user validation and session management

--- PGRFacil.UI/src/hooks/AuthProvider.tsx
+++ PGRFacil.UI/src/hooks/AuthProvider.tsx
@@ -8,54 +8,62 @@ import { toast } from "sonner";
 
 // Provider props type
 interface AuthProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [user, setUser] = useState<Usuario | null>(() => {
-    const savedUser = localStorage.getItem("userData");
-    if (savedUser) {
-      return JSON.parse(savedUser) as Usuario;
-    } else {
-      return null;
-    }
-  });
-
-  const navigate = useNavigate();
-
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("userData");
-    localStorage.removeItem("token");
-  };
-
-  useEffect(() => {
-    // Handle global unauthorized errors. Will listen only once during the app lifecycle.
-    onUnauthorized(() => {
-      console.log("[DEBUG] callback received!");
-      toast.error("Sessão expirada. Por favor, faça login novamente.");
-      logout();
-      navigate("/login");
-    });
-  });
-
-  const login = (loginResponse: LoginResponse) => {
-    const userData: Usuario = {
-      id: "",
-      email: loginResponse.email,
-      permissoes: loginResponse.permissoes,
+    const isUserValid = (obj: unknown): obj is Usuario => {
+        return (
+            typeof obj === "object" &&
+            obj !== null &&
+            typeof (obj as Usuario).id === "string" &&
+            typeof (obj as Usuario).email === "string" &&
+            Array.isArray((obj as Usuario).permissoes)
+        );
     };
-    setUser(userData);
-    localStorage.setItem("userData", JSON.stringify(userData));
-    localStorage.setItem("token", loginResponse.token);
-  };
 
+    const [user, setUser] = useState<Usuario | null>(() => {
+        const savedUser = localStorage.getItem("userData");
+        const parsedUser = JSON.parse(savedUser!);
+        if (isUserValid(parsedUser)) {
+            return parsedUser;
+        } else {
+            localStorage.removeItem("userData");
+            localStorage.removeItem("token");
+            return null;
+        }
+    });
 
-  const isUserEditor = user?.permissoes.includes("Editor") ?? false;
+    const navigate = useNavigate();
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout, isUserEditor }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("userData");
+        localStorage.removeItem("token");
+    };
+
+    useEffect(() => {
+        // Handle global unauthorized errors. Will listen only once during the app lifecycle.
+        onUnauthorized(() => {
+            console.log("[DEBUG] callback received!");
+            toast.error("Sessão expirada. Por favor, faça login novamente.");
+            logout();
+            navigate("/login");
+        });
+    });
+
+    const login = (loginResponse: LoginResponse) => {
+        const userData: Usuario = {
+            id: "",
+            email: loginResponse.email,
+            permissoes: loginResponse.permissoes,
+        };
+        setUser(userData);
+        localStorage.setItem("userData", JSON.stringify(userData));
+        localStorage.setItem("token", loginResponse.token);
+    };
+
+    const isUserEditor = user?.permissoes.includes("Editor") ?? false;
+
+    return <AuthContext.Provider value={{ user, login, logout, isUserEditor }}>{children}</AuthContext.Provider>;
 };
