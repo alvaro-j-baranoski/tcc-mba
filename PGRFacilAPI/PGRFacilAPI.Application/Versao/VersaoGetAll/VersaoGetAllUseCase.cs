019fbf81c274c71b8304d25b51157b36ae9f36fb
Alvaro <alvarojosebaranoski01@gmail.com>
2026-04-02 01:58:42 -0300

    feat: Enhance version comparison

--- PGRFacilAPI/PGRFacilAPI.Application/Versao/VersaoGetAll/VersaoGetAllUseCase.cs
+++ PGRFacilAPI/PGRFacilAPI.Application/Versao/VersaoGetAll/VersaoGetAllUseCase.cs
@@ -12,25 +12,11 @@ namespace PGRFacilAPI.Application.Versao.VersaoGetAll
 
             IEnumerable<VersaoEntity> versoes = await versaoRepository.GetAllByGheId(input.GheId);
             List<VersaoDto> result = versoes
-                .OrderBy(v => v.Versao, new VersaoComparer())
+                .OrderBy(v => new Version(v.Versao))
                 .Select(v => new VersaoDto(v.Id, v.GheId, v.Versao, v.DataCriacao, v.Observacoes))
                 .ToList();
 
             return new VersaoGetAllOutputDto(result);
-        }
-
-        private sealed class VersaoComparer : IComparer<string>
-        {
-            public int Compare(string? x, string? y)
-            {
-                var partsX = (x ?? "0.0").Split('.');
-                var partsY = (y ?? "0.0").Split('.');
-
-                int majorCompare = int.Parse(partsX[0]).CompareTo(int.Parse(partsY[0]));
-                if (majorCompare != 0) return majorCompare;
-
-                return int.Parse(partsX[1]).CompareTo(int.Parse(partsY[1]));
-            }
         }
     }
 }
